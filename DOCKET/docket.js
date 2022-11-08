90fff2fc582eaccf4d885f47c1c2f75287218266
Yash <114616632+yashdahiya0@users.noreply.github.com>
2022-11-08 12:17:59 -0800

    Add files via upload

--- DOCKET/docket.js
+++ DOCKET/docket.js
@@ -1,45 +1,9 @@
 
-const items = [{ "item":"testing", "priority":1, "status":true},
-{"item":"testing", "priority":2, "status":false},
-{"item":"testing", "priority":3, "status":true}]
+//Display the Date
+const dateElement = document.getElementById("date");
+const options = {weekday : "long", month:"short", day:"numeric"};
+const today = new Date();
 
-const itemsStr =JSON.stringify(items);
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
-console.log(itemsStr);
-console.log(items);
 
-function fetchItems(){
-
-    const itemsList = document.querySelector('ul.todo-items');
-    itemsList.innerHTML = '';
-    var newItemHTML = '';
-
-    try{
-        var items = localStorage.getItem('todo-items');
-        var itemsArr = JSON.parse(items);
-        
-        for (var i=0; i<itemsArr.length; i++){
-            var status = '';
-            if(itemsArr[i].status == true){
-                status = 'class="done"';
-            }
-
-            newItemHTML += `<li data-itemindex = "${i}">
-            <span class="item">${itemsArr[i].item}</span> 
-            <div><span class="buttons">✅</span> 
-            <span class="buttons">❌</span></div>
-            </li>`;
-            itemsArr[i]
-        }
-        itemsList.innerHTML = newItemHTML;
-    } catch(error){
-
-    }
-}
-
-function saveItems(obj){
-    var string = JSON.stringify(obj);
-    localStorage.setItem('todo-items', string);
-}
-
-fetchItems();
