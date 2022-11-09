78ecdf3933bbb07a91d46a50d33ad2a052121b6b
Yash <114616632+yashdahiya0@users.noreply.github.com>
2022-11-08 16:05:50 -0800

    Delete DOCKET directory

--- DOCKET/docket.js
+++
@@ -1,9 +0,0 @@
-
-//Display the Date
-const dateElement = document.getElementById("date");
-const options = {weekday : "long", month:"short", day:"numeric"};
-const today = new Date();
-
-dateElement.innerHTML = today.toLocaleDateString("en-US", options);
-
-
